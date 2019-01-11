4a9ac82d32f268c29fc555995a4d8fe3cc0bb02c
Carlos H Brandt <c.brandt@jacobs-university.de>
2019-01-11 13:25:20 +0100

    Add 'Notes' collection to server/main.js

--- vespaapp/server/main.js
+++ vespaapp/server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { onPageLoad } from "meteor/server-render";
 
+import '../imports/api/notes.js';
 import '../imports/api/data.js';
 
 Meteor.startup(() => {
