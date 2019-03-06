7a4cea20f52cb0fb83ce018670d0a75eda5d03bb
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2019-03-06 23:49:02 +0100

    Add 'order' field tp data_index, and sort entries in Home Targets

--- vespaapp/imports/api/collections/data_index.js
+++ vespaapp/imports/api/collections/data_index.js
@@ -6,8 +6,11 @@ export const DataIndex = new Mongo.Collection('data_index');
 if (Meteor.isServer) {
 
   console.log('Publishing: data_index');
+
   Meteor.publish('data_index', () => {
-    return DataIndex.find();
+    return DataIndex.find({}, {sort: [['order','asc'],
+                                      ['hasMap','desc'],
+                                      ['name','asc']] });
   });
 
 }
